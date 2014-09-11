8249620ab5ed20461e16e08a25633d698c741ddc
Hooolm <r.holm.j93@gmail.com>
2014-09-11 11:27:41 +0200

    Simplified main file.

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include "kernel.h"
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
-#include "math.h"
 #include "utility_lcd/utility_lcd.h"
 
 /* nxtOSEK hook to be invoked from an ISR in category 2 */
@@ -10,12 +9,8 @@ void user_1ms_isr_type2(void){ /* do nothing */ }
 
 TASK(OSEK_Task_Background)
 { 
-    while(1)
-    {   
-    
-        display_goto_xy(0,0);
-        display_int(foo(2), 3);
-        display_update();
-    }
-    ecrobot_init_sonar_sensor(NXT_PORT_S2);
+   while(1)
+   {  
+      // Main loop
+   }
 }
