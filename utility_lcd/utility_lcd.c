614be50d6039f48f6702ee97f26e37a778e976dc
Hooolm <r.holm.j93@gmail.com>
2014-09-11 11:27:22 +0200

    Added write_to_lcd_line fucntion to lcd utility file.

--- utility_lcd/utility_lcd.c
+++ utility_lcd/utility_lcd.c
@@ -1,6 +1,24 @@
-#include "utility_lcd.h"  /* Include the header (not strictly necessary here) */
+#include "utility_lcd.h"  
+#include "kernel.h"
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+#include <string.h>
 
-int foo(int x)    /* Function definition */
+/* Writes a string of chars on a given line on the display returns 1 if it succeeds */
+int write_to_lcd_line(int line_number, char* string)
 {
-    return 42;
+   char* output;
+   if(line_number >= 0 && line_number < 8)
+   {  
+         memcpy(output, string, 16);
+      
+         // The arguments was valid
+         display_goto_xy(0, line_number);
+         display_string(output);
+         display_update();
+         return 1;  
+   }
+
+   // The writing was unsuccesful
+   return 0;
 }
