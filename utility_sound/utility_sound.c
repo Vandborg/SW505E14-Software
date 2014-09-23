f37a61cec4741cd4dbfe3417ef221873993799d2
Hooolm <r.holm.j93@gmail.com>
2014-09-23 14:50:46 +0200

    Added a utility for playing sounds for debugging purposes.

--- utility_sound/utility_sound.c
+++ utility_sound/utility_sound.c
@@ -7,16 +7,59 @@
 // System sounds
 void play_sound_startup(void)
 {
-	ecrobot_sound_tone(2000, 100, 60);
-	systick_wait_ms(100);
-	ecrobot_sound_tone(500, 100, 60);
-	systick_wait_ms(100);
-	ecrobot_sound_tone(2000, 100, 60);
+	ecrobot_sound_tone(800, 300, 60);
+	systick_wait_ms(150);
+	ecrobot_sound_tone(1200, 150, 60);
+	systick_wait_ms(150);
+	ecrobot_sound_tone(2000, 150, 60);
+	systick_wait_ms(200);
+	ecrobot_sound_tone(2000, 150, 60);
+}
+
+void play_sound_shutdown(void)
+{
+	ecrobot_sound_tone(2000, 300, 60);
+	systick_wait_ms(150);
+	ecrobot_sound_tone(1200, 150, 60);
+	systick_wait_ms(150);
+	ecrobot_sound_tone(800, 150, 60);
+	systick_wait_ms(200);
+	ecrobot_sound_tone(800, 150, 60);
 }
 
 void play_sound_button_feedback(void)
 {
 	ecrobot_sound_tone(2000, 100, 60);
+}
+
+void play_sound_mode_wait(void)
+{
+	ecrobot_sound_tone(1700, 100, 60);
+	systick_wait_ms(100);
+	ecrobot_sound_tone(1800, 100, 60);
+	systick_wait_ms(100);
+	ecrobot_sound_tone(1700, 100, 100);
+}
+
+void play_sound_mode_select_menu(void)
+{
+	ecrobot_sound_tone(1600, 100, 60);
+	systick_wait_ms(100);
+	ecrobot_sound_tone(1700, 100, 60);
+	systick_wait_ms(100);
+	ecrobot_sound_tone(1800, 200, 100);
+}
+
+void play_sound_mode_selected_default(void)
+{
+	ecrobot_sound_tone(500, 300, 60);
+	systick_wait_ms(500);
+	ecrobot_sound_tone(500, 60, 60);
+	systick_wait_ms(100);
+	ecrobot_sound_tone(500, 60, 60);
+	systick_wait_ms(100);
+	ecrobot_sound_tone(500, 60, 60);
+	systick_wait_ms(100);
 }
 
 void play_sound_end_task(void)
--- utility_sound/utility_sound.h
+++ utility_sound/utility_sound.h
@@ -1,12 +1,20 @@
 #ifndef UTILITY_SOUND_H_   /* Include guard */
 #define UTILITY_SOUND_H_
 
-void play_sound_dsb(void);
-
 void play_sound_startup(void);
+
+void play_sound_shutdown(void);
 
 void play_sound_button_feedback(void);
 
+void play_sound_mode_wait(void);
+
+void play_sound_mode_select_menu(void);
+
+void play_sound_mode_selected_default(void);
+
 void play_sound_end_task(void);
+
+void play_sound_dsb(void);
 
 #endif
