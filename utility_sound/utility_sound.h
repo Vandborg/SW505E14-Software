2b90c890e450111088806bb97fc19e971273f685
feupeu <nkmouritzsen@gmail.com>
2014-09-24 14:23:19 +0200

    Merge all play_sounds functions into single function

--- utility_sound/utility_sound.h
+++ utility_sound/utility_sound.h
@@ -1,22 +1,6 @@
 #ifndef UTILITY_SOUND_H_   /* Include guard */
 #define UTILITY_SOUND_H_
 
-void play_sound_startup(void);
-
-void play_sound_shutdown(void);
-
-void play_sound_button_feedback(void);
-
-void play_sound_mode_wait(void);
-
-void play_sound_mode_select_menu(void);
-
-void play_sound_mode_selected_default(void);
-
-void play_sound_end_task(void);
-
-void play_sound_dsb(void);
-
-void play_sound_mario(void);
+void play_sound(int);
 
 #endif
