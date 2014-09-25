95cb176f7a8a022ab7a0d1ff1ef202d7103bb6e7
Hooolm <r.holm.j93@gmail.com>
2014-09-25 09:52:13 +0200

    Add hook routines to initialize the sensors. Include definitions header file in main.

--- main.c
+++ main.c
@@ -4,12 +4,27 @@
 #include "string.h"
 
 // Own libraries
+#include "utility_definitions/utility_definitions.h"
 #include "utility_mode_handling/utility_mode_handling.h" 
 #include "utility_sound/utility_sound.h"  
 #include "color_scanning/color_scanning.h"
 
-/* nxtOSEK hook to be invoked from an ISR in category 2 */
+// Hook routines
+
+// System clock
 void user_1ms_isr_type2(void) { }
+
+// On device initialization
+void ecrobot_device_initialize(void)
+{
+	ecrobot_init_nxtcolorsensor(COLOR_SENSOR);
+}
+
+// On device termination
+void ecrobot_device_terminate(void)
+{
+	ecrobot_term_nxtcolorsensor(COLOR_SENSOR);
+}
 
 TASK(Task1) 
 { 
