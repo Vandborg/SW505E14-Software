efd4ad542a5b82a48a4499a82c4b5494e3d0efab
Hooolm <r.holm.j93@gmail.com>
2014-09-25 12:10:05 +0200

    Add initialization for sonar sensor in hook routines. And includesonar_sensor program to main.

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "utility_mode_handling/utility_mode_handling.h" 
 #include "utility_sound/utility_sound.h"  
 #include "color_scanning/color_scanning.h"
+#include "sonar_sensor/sonar_sensor.h"
 
 // Hook routines
 
@@ -18,12 +19,14 @@ void user_1ms_isr_type2(void) { }
 void ecrobot_device_initialize(void)
 {
 	ecrobot_init_nxtcolorsensor(COLOR_SENSOR);
+	ecrobot_init_sonar_sensor(SONAR_SENSOR);
 }
 
 // On device termination
 void ecrobot_device_terminate(void)
 {
 	ecrobot_term_nxtcolorsensor(COLOR_SENSOR);
+	ecrobot_term_sonar_sensor(SONAR_SENSOR);
 }
 
 TASK(Task1) 
