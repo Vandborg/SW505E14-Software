87e531573e0f22a7cdcedd10102e85bd247ba37d
Shaderex <meot12@student.aau.dk>
2014-10-02 10:59:41 +0200

    Add comments for utility_braking.c and replace magic numbers.

--- utility/utility_movement/utility_braking.c
+++ utility/utility_movement/utility_braking.c
@@ -1,26 +1,25 @@
+// 3rd party includes
 #include "ecrobot_interface.h"
 #include "kernel.h"
 #include "kernel_id.h"
 #include "string.h"
 
+// Own header include
 #include "utility_braking.h"
+
+// Own library includes
 #include "../utility_definitions/utility_definitions.h"
 
-#define HIGH_BRAKEPOWER 2
-#define MEDIUM_BRAKEPOWER 1
-#define LOW_BRAKEPOWER 0
+// Locally needed definitions
 #define FADING_SPEED_REDUCTION 1.10
 #define SLOW_SPEED_REDUCTION 1.03
 #define MOTOR_LOWER_SPEED_LIMIT 10
 #define MOTOR_NO_SPEED 0
 #define BRAKE 1
 #define NO_BRAKE 0
+#define SMALL_WAIT_MS 100
 
-/* 
- * Function that brakes the forklift.
- * brakepower       The desired brake power. Ranges from 0 to 2, low to hard.
- * forklift_speed   The current speed of the forklift. Ranges from -100 to 100.
-*/
+// Main function that that brakes the forklift. Call this to brake.
 void forklift_brake(int brakepower, int forklift_speed)
 {
     switch(brakepower)
@@ -39,14 +38,15 @@ void forklift_brake(int brakepower, int forklift_speed)
     }
 }
 
-/* Sets both wheel motors to zero speed and applies brake.*/
+// Function that sets both motors to 0 speed and applies brake.
 void fullstop_brake(void)
 {
     nxt_motor_set_speed(LEFT_MOTOR, MOTOR_NO_SPEED, BRAKE);
     nxt_motor_set_speed(RIGHT_MOTOR, MOTOR_NO_SPEED, BRAKE);
-    systick_wait_ms(100);
+    systick_wait_ms(SMALL_WAIT_MS);
 }
 
+// Function that reduces both motors' speed gradually from current speed to 0. 
 void fading_brake(int brakepower, int forklift_speed)
 {
     if(forklift_speed > MOTOR_NO_SPEED)
@@ -59,7 +59,7 @@ void fading_brake(int brakepower, int forklift_speed)
             nxt_motor_set_speed(RIGHT_MOTOR, 
                                 forklift_speed / FADING_SPEED_REDUCTION,
                                 BRAKE);
-            systick_wait_ms(100);
+            systick_wait_ms(SMALL_WAIT_MS);
             forklift_speed = forklift_speed / FADING_SPEED_REDUCTION;
         }
         fullstop_brake();
@@ -75,7 +75,7 @@ void fading_brake(int brakepower, int forklift_speed)
             nxt_motor_set_speed(RIGHT_MOTOR, 
                                 forklift_speed / FADING_SPEED_REDUCTION,
                                 BRAKE);
-            systick_wait_ms(100);
+            systick_wait_ms(SMALL_WAIT_MS);
             forklift_speed = forklift_speed / FADING_SPEED_REDUCTION;
         }
         fullstop_brake();
@@ -86,6 +86,7 @@ void fading_brake(int brakepower, int forklift_speed)
     }
 }
 
+//  Function that reduces both motors' speed slowly from current speed to 0.
 void slow_brake(int brakepower, int forklift_speed)
 {
     if(forklift_speed > MOTOR_NO_SPEED)
@@ -98,7 +99,7 @@ void slow_brake(int brakepower, int forklift_speed)
             nxt_motor_set_speed(RIGHT_MOTOR, 
                                 forklift_speed / SLOW_SPEED_REDUCTION,
                                 BRAKE);
-            systick_wait_ms(100);
+            systick_wait_ms(SMALL_WAIT_MS);
             forklift_speed = forklift_speed / SLOW_SPEED_REDUCTION;
         }
 
@@ -115,7 +116,7 @@ void slow_brake(int brakepower, int forklift_speed)
             nxt_motor_set_speed(RIGHT_MOTOR, 
                                 forklift_speed / SLOW_SPEED_REDUCTION,
                                 BRAKE);
-            systick_wait_ms(100);
+            systick_wait_ms(SMALL_WAIT_MS);
             forklift_speed = forklift_speed / SLOW_SPEED_REDUCTION;
         }
         fullstop_brake();
