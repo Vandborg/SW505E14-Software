c63348dbdcad82247c95e58d118f5a752b48a74b
Shaderex <meot12@student.aau.dk>
2014-10-07 12:51:39 +0200

    Add to cleanup and comment utility_rotate_degrees c + h

--- utility/utility_movement/utility_rotate_degrees.c
+++ utility/utility_movement/utility_rotate_degrees.c
@@ -8,29 +8,23 @@
 
 // Own libraries
 #include "utility/utility_definitions/utility_definitions.h"
-#include "utility/utility_sound/utility_sound.h"
-#include "utility/utility_lcd/utility_lcd.h"
-#include "utility/utility_string/utility_string.h"
 
+// Function that rotates one engine a specific amount of degrees at given speed
 void motor_rotate_degrees(int degrees, int speed, char* motor)
 {
-    char* placeholder = null;
-    int current_rotations = nxt_motor_get_count(motor);
     
-
+    int current_rotations = nxt_motor_get_count(motor);
     
     // Checks if the desired direction is forward.
     if(speed > MOTOR_NO_SPEED)
     {
         int target_rotations = current_rotations + degrees;
+    
         // While the condition still holds, keep moving the fork up
         while(target_rotations > current_rotations)
         {
             nxt_motor_set_speed(motor, speed, BRAKE);
             current_rotations = nxt_motor_get_count(motor);
-            placeholder = int_to_string(current_rotations, placeholder);
-            lcd_display_line(LCD_LINE_ONE, placeholder, TRUE);
-            systick_wait_ms(50);
         }
 
     }
@@ -38,14 +32,12 @@ void motor_rotate_degrees(int degrees, int speed, char* motor)
     else if(speed < MOTOR_NO_SPEED)
     {
         int target_rotations = current_rotations - degrees;
+    
         // While the condition still holds, keep moving the fork down
         while(target_rotations < current_rotations)
         {
             nxt_motor_set_speed(motor, speed, BRAKE);
             current_rotations = nxt_motor_get_count(motor);
-            placeholder = int_to_string(current_rotations, placeholder);
-            lcd_display_line(LCD_LINE_ONE, placeholder, TRUE);
-            systick_wait_ms(50);
         }        
     }
 
@@ -56,8 +48,8 @@ void motor_rotate_degrees(int degrees, int speed, char* motor)
 // Turns a specific motor a given amount of rotations at a specific speed.
 // The mode can be used to indicate how many fractions of a whole rotation you
 // want to turn. Defines are made for this in utility_definions.h. 
-// Use this to avoid having to compute how many degrees a specific amount of
-// rotations are on your own. 
+// Use this to avoid having to compute how many degrees correspond to a specific
+// amount of rotations on your own. 
 void motor_rotate_rotations(int rotations, int mode, int speed, char* motor)
 {
     motor_rotate_degrees((rotations*360)/mode, speed, motor);
--- utility/utility_movement/utility_rotate_degrees.h
+++ utility/utility_movement/utility_rotate_degrees.h
@@ -1,7 +1,31 @@
 #ifndef UTILITY_ROTATE_DEGREES_H_   /* Include guard */
 #define UTILITY_ROTATE_DEGREES_H_
 
+/*
+ * Function that handles rotating a single motor a specific amount of degrees
+ * with a specific speed
+ * Returns nothing
+ * Takes 3 parameters:
+ * Degrees       The amount of degrees you want to rotate the motor
+ * Speed         The speed you want to rotate the motor at
+ * Motor         The name of the motor you want to turn. LEFT_MOTOR/RIGHT_MOTOR
+ */
 void motor_rotate_degrees(int degrees, int speed, char* motor);
+
+
+
+
+/*
+ * Function that calls the degrees function, but takes an amount of rotations
+ * as input instead. Can also use the mode variable to specify fraction turns.
+ * Returns nothing
+ * Takes 4 parameters:
+ * Degrees       The amount of degrees you want to rotate the motor
+ * Mode          The fraction type you want to use. whole, half, quarters etc.
+ *               they are specified in utility_definitions.h.
+ * Speed         The speed you want to rotate the motor at
+ * Motor         The name of the motor you want to turn. LEFT_MOTOR/RIGHT_MOTOR
+ */
 void motor_rotate_rotations(int rotations, int mode, int speed, char* motor);
 
 #endif
