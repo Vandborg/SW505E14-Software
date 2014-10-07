2a35ae4d9e637063a41370fbba44dad6c8fd896c
Shaderex <meot12@student.aau.dk>
2014-10-07 15:11:38 +0200

    Add to refactor utility_rotate_degrees.c

--- utility/utility_movement/utility_rotate_degrees.c
+++ utility/utility_movement/utility_rotate_degrees.c
@@ -9,40 +9,76 @@
 // Own libraries
 #include "utility/utility_definitions/utility_definitions.h"
 
+#define TURN_TIMEOUT 100
+#define DIRECTION 1
+#define REVERSE_DIRECTION -1
+
 // Function that rotates one engine a specific amount of degrees at given speed
 void motor_rotate_degrees(int degrees, int speed, U32 motor)
 {
-    
-    int current_rotations = nxt_motor_get_count(motor);
-    
-    // Checks if the desired direction is forward.
-    if(speed > MOTOR_NO_SPEED)
+    // Get the starting degrees for the motor, set current to the same value.
+    int start_degrees = nxt_motor_get_count(motor);
+    int current_degrees = start_degrees;
+
+    // Initialize direction
+    int direction = 0;
+
+    // Check whether both degrees and speed are either positive or negative.
+    // If they are, set direction to forward, else direction is backward.
+    if((degrees < 0 && speed < 0) || (degrees > 0 && speed > 0))
     {
-        int target_rotations = current_rotations + degrees;
-    
-        // While the condition still holds, keep moving the fork up
-        while(target_rotations > current_rotations)
+        direction = DIRECTION;
+    }
+    else
+    {
+        direction = REVERSE_DIRECTION;
+    }
+
+    // Checks if the speed is set to 0. If so, skip the entire thing.
+    if(speed != MOTOR_NO_SPEED)
+    {
+
+        // Numeric value of speed
+        if(speed < 0)
         {
-            nxt_motor_set_speed(motor, speed, BRAKE);
-            current_rotations = nxt_motor_get_count(motor);
+            speed *= -1;
         }
 
-    }
-    // Checks if the desired direction is backward. 
-    else if(speed < MOTOR_NO_SPEED)
-    {
-        int target_rotations = current_rotations - degrees;
-    
-        // While the condition still holds, keep moving the fork down
-        while(target_rotations < current_rotations)
+        // Numeric value of degrees
+        if(degrees < 0)
         {
-            nxt_motor_set_speed(motor, speed, BRAKE);
-            current_rotations = nxt_motor_get_count(motor);
-        }        
+            degrees *= -1;
+        }
+
+        // Compute the target degrees for the rotation.
+        int target_degrees = start_degrees + degrees * direction;
+        
+        // Since we have |speed|, multiply by the desired direction.
+        speed *= direction;
+
+        // Checks if the target is after the current - forward direction
+        if(target_degrees > current_degrees)
+        {
+            while(target_degrees > current_degrees)
+            {
+                nxt_motor_set_speed(motor, speed, BRAKE);
+                current_degrees = nxt_motor_get_count(motor);
+            }   
+        }
+        // Checks if the target is before the current - reverse direction
+        else if(target_degrees < current_degrees)
+        {
+            while(target_degrees < current_degrees)
+            {
+                nxt_motor_set_speed(motor, speed, BRAKE);
+                current_degrees = nxt_motor_get_count(motor);
+            }
+        }
     }
 
-    // Once the desired height has been reached, stop the motor.
+    // Brake when the rotation has been complete.
     nxt_motor_set_speed(motor, MOTOR_NO_SPEED, BRAKE);
+    systick_wait_ms(TURN_TIMEOUT);
 }
 
 // Turns a specific motor a given amount of rotations at a specific speed.
