24e82f9d2c4d40435301014fa1d5d14fd7d78627
Shaderex <meot12@student.aau.dk>
2014-10-07 10:20:33 +0200

    Add comments to utility_fork.h

--- utility/utility_fork/utility_fork.h
+++ utility/utility_fork/utility_fork.h
@@ -3,7 +3,8 @@
 
 /*
  * Function that checks which lift height is desired and calls the
- * lifting function with a converted value.
+ * lifting function with a converted value. 
+ * Call this to move the fork.
  * Returns nothing
  * Takes 1 parameter:
  * lifting_mode     int that ranges from 0 to 2. Defines low, medium or max hght
@@ -12,6 +13,7 @@ void operate_fork(int lifting_mode);
 
 /*
  * Function that either lifts or lowers the fork to a desired height.
+ * Do not call this
  * Returns nothing
  * Takes 1 parameter:
  * lifting_height    int that specifies the desired height. 
