7452079d297c20cd8b3f3a7af2ac62965dc5e31a
Jesper Vandborg <jesper@vandborg>
2014-10-09 08:38:47 +0200

    add correct indent to header files

--- utility/utility_movement/utility_distance.h
+++ utility/utility_movement/utility_distance.h
@@ -1,20 +1,20 @@
 #ifndef UTILITY_DISTANCE_H_   /* Include guard */
-#define UTILITY_DISTANCE_H_
+    #define UTILITY_DISTANCE_H_
 
-// Include definitions
-#include "utility/utility_definitions/utility_definitions.h"
+    // Include definitions
+    #include "utility/utility_definitions/utility_definitions.h"
 
-/* 
-* Resets the driving motors count and thereby the distance driven
-* Takes no parameters.
-*/
-void reset_distance(void);
+    /* 
+    * Resets the driving motors count and thereby the distance driven
+    * Takes no parameters.
+    */
+    void reset_distance(void);
 
-/* 
-* Returns the average distance of the two motors in cm.
-* Takes no parameters.
-* Returns an integer describing the distance in cm.
-*/
-int current_distance(void);
+    /* 
+    * Returns the average distance of the two motors in cm.
+    * Takes no parameters.
+    * Returns an integer describing the distance in cm.
+    */
+    int current_distance(void);
 
 #endif
--- utility/utility_movement/utility_drive_straight.h
+++ utility/utility_movement/utility_drive_straight.h
@@ -1,10 +1,10 @@
 #ifndef UTILITY_DRIVE_STRAIGHT_H_   /* Include guard */
-#define UTILITY_DRIVE_STRAIGHT_H_
+    #define UTILITY_DRIVE_STRAIGHT_H_
 
-/*
- * Makes the truck move forward without displaced start
- * Takes no parameters
- */
-void drive_forward(void);
+    /*
+     * Makes the truck move forward without displaced start
+     * Takes no parameters
+     */
+    void drive_forward(void);
 
 #endif
