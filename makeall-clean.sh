17a727e65bc7e3bb15431f6bc28800c25bc38c5c
Jesper Vandborg <jesper@vandborg>
2014-10-14 09:40:12 +0200

    add auto gen of Makefile

--- makeall-clean.sh
+++ makeall-clean.sh
@@ -4,8 +4,26 @@ echo "Cleaing"
 echo "------------------"
 
 rm -R build
-
 echo "------------------"
+
+echo "Generating Makefile"
+echo "------------------"
+
+var="TARGET = main_OSEK\n"
+var="${var}TARGET_SOURCES = "
+echo -e -n "$var" > Makefile
+find=$(find -type f -iname "*.c" ! -iname "kernel_cfg.c" ! -iname "utility_movement.c")
+echo $find | tr '\n' ' ' >> Makefile
+
+var="TOPPERS_OSEK_OIL_SOURCE = ./main.oil"
+echo -e "\n$var" >> Makefile
+
+var="O_PATH ?= build"
+echo -e "\n$var" >> Makefile
+var="include ../ecrobot/ecrobot.mak"
+echo -e "\n$var" >> Makefile
+echo "------------------"
+
 echo "Initializing build"
 echo "------------------"
 
--- makeall.sh
+++ makeall.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
+
+cd /nxtOSEK/SW505E14_Software/
+
+echo "Generating Makefile"
+echo "------------------"
+
+var="TARGET = main_OSEK\n"
+var="${var}TARGET_SOURCES = "
+echo -e -n "$var" > Makefile
+find=$(find -type f -iname "*.c" ! -iname "kernel_cfg.c" ! -iname "utility_movement.c")
+echo $find | tr '\n' ' ' >> Makefile
+
+var="TOPPERS_OSEK_OIL_SOURCE = ./main.oil"
+echo -e "\n$var" >> Makefile
+
+var="O_PATH ?= build"
+echo -e "\n$var" >> Makefile
+var="include ../ecrobot/ecrobot.mak"
+echo -e "\n$var" >> Makefile
+echo "------------------"
+
 echo "Initializing build"
 echo "------------------"
 
-cd /nxtOSEK/SW505E14_Software/
 make all
 
 echo "------------------"
