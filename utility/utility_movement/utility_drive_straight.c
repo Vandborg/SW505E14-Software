5bfc57dc2e7ab34e1425df7bf2a2628fcdaa015e
Jesper Clausen <jclau12@student.aau.dk>
2014-10-20 08:47:24 +0200

    Add drive_straight_pid

--- utility/utility_movement/utility_drive_straight.c
+++ utility/utility_movement/utility_drive_straight.c
@@ -20,3 +20,89 @@ void drive_forward(void)
     nxt_motor_set_speed(LEFT_MOTOR, SPEED, TRUE);
     nxt_motor_set_speed(RIGHT_MOTOR, SPEED, TRUE);
 }
+
+void drive_straight_pid(void) {
+    int Kp = 18000;
+    int Ki = 1;
+    int Kd = 40000;
+    int Tp = 80;
+
+    int integral = 0;
+    int currentError = 0;
+    int lastError = 0;
+    int derivative = 0;
+    int turn = 0;
+
+    int powerA = 0;
+    int powerB = 0;
+    int right_wheel_count = 0;
+    int left_wheel_count = 0;
+    int intial_rotation_right = nxt_motor_get_count(RIGHT_MOTOR);
+    int intial_rotation_left = nxt_motor_get_count(LEFT_MOTOR);
+
+    int initial_time = systick_get_ms();
+    int current_time = 0;
+
+    reset_distance();
+
+    int flag = 1;
+
+    while(true) 
+    {
+        if (flag)
+        {
+            left_wheel_count = 
+            nxt_motor_get_count(LEFT_MOTOR) - intial_rotation_left;
+            right_wheel_count = 
+                nxt_motor_get_count(RIGHT_MOTOR) - intial_rotation_right;
+            
+            currentError = right_wheel_count - left_wheel_count;
+            
+            integral = integral + currentError;
+
+            derivative = currentError - lastError;
+            turn = Kp * currentError + Ki * integral + Kd * derivative;
+            turn = turn / 10000; // ---> This is only needed if the k's 
+                                 //      are multiplied by a hundred
+            powerA = Tp + turn;
+            powerB = Tp - turn;
+
+            // Make motors go... 
+            nxt_motor_set_speed(LEFT_MOTOR, powerA, 1);
+            nxt_motor_set_speed(RIGHT_MOTOR, powerB, 1);
+
+            lastError = currentError;
+
+            current_time = systick_get_ms() - initial_time;
+        }
+        else 
+        {
+            right_wheel_count = 
+                nxt_motor_get_count(RIGHT_MOTOR) - intial_rotation_right;
+            left_wheel_count = 
+                nxt_motor_get_count(LEFT_MOTOR) - intial_rotation_left;
+            
+            currentError = right_wheel_count - left_wheel_count;
+            
+            integral = integral + currentError;
+
+            derivative = currentError - lastError;
+            turn = Kp * currentError + Ki * integral + Kd * derivative;
+            turn = turn / 10000; // ---> This is only needed if the k's 
+                                 //      are multiplied by a hundred
+            powerA = Tp + turn;
+            powerB = Tp - turn;
+
+            // Make motors go... 
+            nxt_motor_set_speed(RIGHT_MOTOR, powerB, 1);
+            nxt_motor_set_speed(LEFT_MOTOR, powerA, 1);
+
+            lastError = currentError;
+
+            current_time = systick_get_ms() - initial_time;
+        }
+        
+        flag = !flag;
+    }
+}
+    
