df852c3ec2f3d51da8ef9baad0279ed6c499307d
feupeu <nkmouritzsen@gmail.com>
2014-10-21 10:27:02 +0200

    Add file to store all global variables

---
+++ utility/utility_variables/utility_variables.h
@@ -0,0 +1,10 @@
+#ifndef UTILITY_VARIABLES_H_
+    #define UTILITY_VARIABLES_H_
+
+    // Include definitions
+    #include "utility/utility_definitions/utility_definitions.h"
+
+    // Include structs
+    #include "utility/utility_structs/utility_structs.h"
+
+#endif
