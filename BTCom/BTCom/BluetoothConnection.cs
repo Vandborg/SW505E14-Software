0ed7865f2d76c29c669b7ee6c5e4044b7d22a717
Hooolm <r.holm.j93@gmail.com>
2014-11-04 13:23:37 +0100

    Add semantics and start to consummation to the package type TYPE_UDPATE_COLOR.

--- BTCom/BTCom/BluetoothConnection.cs
+++ BTCom/BTCom/BluetoothConnection.cs
@@ -10,35 +10,54 @@ namespace BTCom
     class BluetoothConnection : SerialPort
     {
         // Delimeter bytes
-        private static byte START_BYTE = 0x2;
-        private static byte END_BYTE = 0x3;
-        
-        // Type bytes
-        private static byte TYPE_READ_COLOR = 0x41;
-        private static byte TYPE_UPDATE_COLOR = 0x42;
-        private static byte TYPE_REPORT_OBSTACLE = 0x43;
-        private static byte TYPE_UPDATE_STATUS = 0x44;
-        private static byte TYPE_FETCH_PALLET = 0x45;
-        private static byte TYPE_DELIVER_PALLET = 0x46;
-        private static byte TYPE_SEND_COLOR = 0x47;
+        const byte START_BYTE = 0x2;
+        const byte END_BYTE = 0x3;
 
+        // Type bytes
+        const byte TYPE_UPDATE_COLOR = 0x41;
+        const byte TYPE_SAVE_COLOR = 0x42;
+        const byte TYPE_REPORT_OBSTACLE = 0x43;
+        const byte TYPE_UPDATE_STATUS = 0x44;
+        const byte TYPE_FETCH_PALLET = 0x45;
+        const byte TYPE_DELIVER_PALLET = 0x46;
+        const byte TYPE_SEND_COLOR = 0x47;
 
         public BluetoothConnection(string portName)
             : base(portName)
         {
-            this.Open();
-            this.ReadTimeout = 10000;
-            this.WriteTimeout = 10000;
+            // If the connection is not established try to connect.
+            Console.WriteLine("[BT]: Attempting to connect.");
+            while (!this.IsOpen)
+            {
+                try
+                {
+                    this.Open(); // Try to open the connection    
+                }
+                catch
+                {
+                    // Catched by retrying after sleep
+                    Console.WriteLine("[BT]: Connection failed. Attempting to connect.");
+                    System.Threading.Thread.Sleep(500);
+                }
+            }
+
+            // Tell the user that the connection was established
+            Console.WriteLine("[BT]: Connection established.");
+
+            this.ReadTimeout = 10000; // Wait 10 sec before timeout on readbuffer
+            this.WriteTimeout = 10000; // Wait 10 sec before timeout on writebuffer
         }
 
+        // Consumes all packages on the buffer one at the time
         public void ConsumePackages()
         {
             byte[] btInputBuffer = ReadBTBuffer(); // The input on the buffer
             ConsumePackages(btInputBuffer); // Consume the buffer
         }
 
+        // Consumes all packages on the buffer one at the time
         private void ConsumePackages(byte[] btInputBuffer)
-        {   
+        {
             int endIndexOfPackage = 0; // The index where the package ends
 
             // Run through the array of bytes on the buffer
@@ -46,8 +65,8 @@ namespace BTCom
             {
                 if (btInputBuffer[i] == END_BYTE)
                 {
-                    byte[] package = new byte[i+1]; // Allocate space for a pacakge
-                    Array.Copy(btInputBuffer, package, i+1); // Copies a package out
+                    byte[] package = new byte[i + 1]; // Allocate space for a pacakge
+                    Array.Copy(btInputBuffer, package, i + 1); // Copies a package out
                     ConsumePackage(package); // Consume a package
                     endIndexOfPackage = i; // Sets the lates index to be the one from where we want to 
                     break;
@@ -60,7 +79,7 @@ namespace BTCom
                 // Allocate space for a new array to be splitted
                 int newInputLength = btInputBuffer.Length - endIndexOfPackage;
                 byte[] newBtInputBuffer = new byte[newInputLength];
-                
+
                 // Copies the rest of the array 
                 Array.Copy(btInputBuffer, endIndexOfPackage + 1, newBtInputBuffer, 0, newInputLength);
 
@@ -70,20 +89,21 @@ namespace BTCom
 
         }
 
-        private int SendPackageBT(byte package_type, char[] package_data)
+        // Sends a package with a given type
+        public int SendPackageBT(byte package_type, byte[] package_data)
         {
-	        // Calculate the size of package_data
+            // Calculate the size of package_data
             int package_data_size = package_data.Length;
 
             // Calculate the total package size, assuming the package data size and
             // start, end, and type bytes.
             int package_total_size = (package_data_size + 3);
-    
+
             // Check whether total data size is bigger than 256 bytes.
-            if(package_total_size > 256)
+            if (package_total_size > 256)
             {
                 // TODO: Send an error code for too big data array
-                return 0;  
+                return 0;
             }
 
             // Make an array that is as large as the total data size of the package.
@@ -91,16 +111,16 @@ namespace BTCom
 
             // Putting the start, end, and type byte in the array.
             package_to_send[0] = START_BYTE;
-	        package_to_send[1] = package_type;
-            package_to_send[package_total_size-1] = END_BYTE;
+            package_to_send[1] = package_type;
+            package_to_send[package_total_size - 1] = END_BYTE;
 
-	        // Fill the package data into the array at the correct array index
-	        for(int i = 0; i < package_data_size; i++)
-	        {
-		        package_to_send[i+2] = (byte) package_data[i];
-	        }
+            // Fill the package data into the array at the correct array index
+            for (int i = 0; i < package_data_size; i++)
+            {
+                package_to_send[i + 2] = (byte)package_data[i];
+            }
 
-	        // Send the request over BT
+            // Send the request over BT
             this.Write(package_to_send, 0, package_total_size);
 
             // Wait to ensure that continous requests is possible
@@ -109,18 +129,73 @@ namespace BTCom
             return 1;
         }
 
+        // Reads the bytes on the buffer
         private byte[] ReadBTBuffer()
         {
+            // The amount of bytes that is on the readbuffer
             int bytesToRead = this.BytesToRead;
-            byte[] input = new byte[bytesToRead];
-            this.Read(input, 0, this.BytesToRead);
 
+            // Allocate array with enough space according to the amount of bytes
+            byte[] input = new byte[bytesToRead];
+
+            // Read from the buffer
+            this.Read(input, 0, bytesToRead);
+
+            // Return the byte array from the buffer
             return input;
         }
 
+        // Consumes one package 
         private void ConsumePackage(byte[] package)
         {
-            // TODO: Consume a package
+            // Allocates data from the package
+            byte[] data = new byte[package.Length - 3];
+
+            // Finds the type of pakcage
+            byte byteType = package[1];
+
+            // Fills the data array with the corresponding data
+            for (int i = 2, j = 0; i < package.Length - 1; i++, j++)
+            {
+                data[j] = package[i];
+            }
+
+            // Convert the data to a string 
+            string dataString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+            // Find the correct method to consume the package
+            switch (byteType)
+            {
+                case TYPE_UPDATE_COLOR:
+
+                    /*  Tell the user that the color is being fetched 
+                     *  dataString contains the id of the color
+                     */
+                    Console.WriteLine("[BT]: Fetching color for \"" + dataString + "\"");
+
+                    // TODO: Get the actual color
+
+                    // Return the colors (three chars foreach color)
+                    string colorString = "255000000";
+
+                    // Convert the color string to byte array
+                    byte[] returnData = Encoding.ASCII.GetBytes(colorString);
+
+                    // Send the package back to the NXT
+                    SendPackageBT(TYPE_SEND_COLOR, returnData);
+
+                    break;
+                case TYPE_SAVE_COLOR:
+                    // 
+
+                    break;
+                case TYPE_REPORT_OBSTACLE:
+                    // TODO
+                    break;
+                case TYPE_UPDATE_STATUS:
+                    // TODO
+                    break;
+            }
             return;
         }
     }
