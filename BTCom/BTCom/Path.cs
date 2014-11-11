60179f804c860a2df490fcdec0493692ebe2d241
feupeu <nkmouritzsen@gmail.com>
2014-11-11 09:31:52 +0100

    Path is now DataEntry

--- BTCom/BTCom/Path.cs
+++ BTCom/BTCom/Path.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace BTCom
 {
-    public class Path
+    public class Path : DataEntry
     {
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;   // Compare reference to null
+            if (obj.GetType() != GetType()) return false;   // Compare types
+            if (ReferenceEquals(this, obj)) return true;    // Compare reference to each other
+
+            var other = (Path) obj;
+
+            // Compare properties of both objects
+            var sameIdentifier = this.Identifier == other.Identifier;
+
+            // Check if all properties are the same
+            return sameIdentifier;
+        }
     }
 }
