1dd9fcbc53c1e4c237f86ee4ad5ed7410435fa80
Shaderex <meot12@student.aau.dk>
2014-11-11 13:48:11 +0100

    Make Navigation and Status global variables.

--- utility/utility_variables/utility_variables.h
+++ utility/utility_variables/utility_variables.h
@@ -9,6 +9,7 @@
 
     // Color values for the sensors
     extern color Colors[AMOUNT_OF_COLORS];
-    extern navigation Navigation;
+    extern navigation Navigation = {"", -1, 0};
+    extern char Status = IDLE; 
 
 #endif
