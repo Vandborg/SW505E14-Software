1618de1a7dfa5fbefd15ac7b37d66a09dde8805b
feupeu <nkmouritzsen@gmail.com>
2014-11-12 14:42:33 +0100

    Add NodeException class to handle exceptions about nodes

---
+++ BTCom/BTCom/Exceptions/NodeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCom.Exceptions
+{
+    public class NodeException : System.Exception
+    {
+        public NodeException() : base()
+        {
+        }
+
+        public NodeException(String s) : base(s)
+        {
+        }
+
+        public NodeException(Node n, String s) : this(n.ToString() + s)
+        {
+        }
+    }
+}
