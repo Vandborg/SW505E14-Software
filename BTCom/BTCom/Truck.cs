d42c6220173089ba1dd0aede79d5b6d08cb37ca3
Jesper Vandborg <jesper@vandborg>
2014-11-13 15:52:27 +0100

    add a singleton of the truck. This should be used for to save the state of the truck.

---
+++ BTCom/BTCom/Truck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCom
+{
+    public sealed class Truck
+    {
+        static Truck _instance = null;
+        static readonly object padlock = new object();
+        private Node _previousNode = new Node("B");
+
+        static Truck(){ }
+
+        public Node PreviousNode
+        {
+            get { return _previousNode; }
+            set { _previousNode = value; }
+        }
+
+        public static Truck Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Truck();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
