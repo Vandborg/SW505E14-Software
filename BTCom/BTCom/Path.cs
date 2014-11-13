aeaf4a43410f2d68bfd8d21fc1e55e94c28e5a48
Jesper Vandborg <jesper@vandborg>
2014-11-13 15:51:03 +0100

    add navigationtable to path

--- BTCom/BTCom/Path.cs
+++ BTCom/BTCom/Path.cs
@@ -10,6 +10,8 @@ namespace BTCom
     {
         private List<Node> _path = new List<Node>();
 
+        private static string[,] navigationTable = new string[4, 4] { {"","L","F","R"}, {"R","","L","F"},
+                                                                      {"F","R","","L"}, {"L","F","R",""} };
         public List<Node> Nodes
         {
             get { return _path; }
@@ -31,6 +33,42 @@ namespace BTCom
 
             // Check if all properties are the same
             return sameIdentifier && samePathLength && samePathNodes;
+        }
+
+        public string[] convertToCarPath()
+        {
+            var carPath = new string[Nodes.Count];
+            //This is not working, but got the gist of it.
+            /*KeyValuePair<Node, Edge> previousNodeKeyvalue = new KeyValuePair<Node, Edge>();
+
+            for (int i = 0; i < Nodes.Count-1; i++)
+            {
+                int comingFromNodeIndex;
+                var nodeOne = Nodes[i];
+                var nodeTwo = Nodes[i+1];
+                //Console.WriteLine(nodeOne);
+                //Console.WriteLine(nodeTwo);
+                var nodeOneKeyvalue = nodeOne.Neighbours.Single(x => nodeTwo.Equals(x.Key));
+                var nodeTwoKeyvalue = nodeTwo.Neighbours.Single(x => nodeOne.Equals(x.Key));
+
+                if (previousNodeKeyvalue.Key == null)
+                {
+                    comingFromNodeIndex = nodeOne.Neighbours.IndexOf(nodeOneKeyvalue);
+                }
+                else
+                {
+                    comingFromNodeIndex = nodeOne.Neighbours.IndexOf(previousNodeKeyvalue);
+                }
+
+                previousNodeKeyvalue = nodeOneKeyvalue;
+
+                Console.WriteLine(nodeOne.Neighbours.IndexOf(nodeOneKeyvalue) + ", " + nodeTwo.Neighbours.IndexOf(nodeTwoKeyvalue));
+                carPath[i] =
+                    navigationTable[
+                        comingFromNodeIndex, nodeTwo.Neighbours.IndexOf(nodeTwoKeyvalue)];
+            }*/
+
+            return carPath;
         }
 
         public override string ToString()
