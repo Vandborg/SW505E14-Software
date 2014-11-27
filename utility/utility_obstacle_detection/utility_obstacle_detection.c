9ed8967a1cb56950f577ddf4e9e0eaac1d6346f9
feupeu <nkmouritzsen@gmail.com>
2014-11-27 14:10:10 +0100

    Add obstacle_detection task

--- utility/utility_obstacle_detection/utility_obstacle_detection.c
+++ utility/utility_obstacle_detection/utility_obstacle_detection.c
@@ -6,34 +6,45 @@
 #include "string.h"
 
 // Own header
-
+#include "utility/utility_obstacle_detection/utility_obstacle_detection.h"
 
 // Own libraries
 #include "utility/utility_lcd/utility_lcd.h"
 #include "utility/utility_variables/utility_variables.h"
-#include "utility/utility_string/utility_string.h"
 #include "utility/utility_structs/utility_structs.h"
+#include "utility/utility_sound/utility_sound.h"
 #include "utility/utility_bluetooth/utility_bluetooth.h"
 
-// DeclareTask(TASK_detect_obstacle);
+DeclareTask(TASK_obstacle_detection);
 
-// S32 distance_left = 0;
-// S32 distance_right = 0;
-// int measurements = 0;
+S32 distance_front = 0;
+S32 distance_rear = 0;
 
-// // This is unfinished. Complete the task to make the object detection
-// // functionality
-// TASK(TASK_detect_obstacle)
-// {
-    
-//     distance_left += ecrobot_get_sonar_sensor(SONAR_SENSOR_FRONT);
-//     distance_right += ecrobot_get_sonar_sensor(SONAR_SENSOR_REAR);
+TASK(TASK_obstacle_detection)
+{
+    distance_front += ecrobot_get_sonar_sensor(SONAR_SENSOR_FRONT);
+    distance_rear += ecrobot_get_sonar_sensor(SONAR_SENSOR_REAR);
 
-//     // Calculate average of new and previous
-//     distance_left /= 2;
-//     distance_right /= 2;
-//     TerminateTask();
-// }
+    distance_front /= 2;
+    distance_rear /= 2;
 
+    play_sound(SOUND_NOTIFICATION);
+
+    if(distance_front >= OBSTACLE_DISTANCE_THRESHOLD)
+    {
+        nxt_motor_set_speed(LEFT_MOTOR, 0, 1);
+        nxt_motor_set_speed(RIGHT_MOTOR, 0, 1);
+        play_sound(SOUND_NOTIFICATION);
+    }
+
+    if(distance_rear >= OBSTACLE_DISTANCE_THRESHOLD)
+    {
+        nxt_motor_set_speed(LEFT_MOTOR, 0, 1);
+        nxt_motor_set_speed(RIGHT_MOTOR, 0, 1);
+        play_sound(SOUND_NOTIFICATION);
+    }
+
+    TerminateTask();
+}
 
 
---
+++ utility/utility_obstacle_detection/utility_obstacle_detection.h
@@ -0,0 +1,9 @@
+#ifndef UTILITY_OBSTACLE_DETECTION_H_   /* Include guard */
+    #define UTILITY_OBSTACLE_DETECTION_H_
+
+    // Include definitions
+    #include "utility/utility_definitions/utility_definitions.h"
+
+    #define OBSTACLE_DISTANCE_THRESHOLD 15
+
+#endif
