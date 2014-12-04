26e91b02306f0e59dd9bbf17de75fb9cca880c7d
Niklas Kirk Mouritzsen <nkmouritzsen@gmail.com>
2014-12-04 14:10:36 +0100

    Add constructor to JobException class

--- BTCom/BTCom/Exceptions/JobException.cs
+++ BTCom/BTCom/Exceptions/JobException.cs
@@ -4,6 +4,9 @@ namespace BTCom.Exceptions
 {
     public class JobException : Exception
     {
+        private Job Job { get; set; }
+        private Pallet Pallet { get; set; }
+
         public JobException() : base()
         {
         }
@@ -12,8 +15,14 @@ namespace BTCom.Exceptions
         {
         }
 
-        public JobException(Job j, String s) : this(j.ToString() + s)
+        public JobException(Job j, String s) : this(s)
         {
+            Job = j;
+        }
+
+        public JobException(Job j, Pallet p, String s) : this(j, s)
+        {
+            Pallet = p;
         }
     }
 }
