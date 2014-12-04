2e1a0d8f89d6a25bc89cd3403142338281aea736
Niklas Kirk Mouritzsen <nkmouritzsen@gmail.com>
2014-12-04 13:57:59 +0100

    Add JobException class

---
+++ BTCom/BTCom/Exceptions/JobException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BTCom.Exceptions
+{
+    public class JobException : Exception
+    {
+        public JobException() : base()
+        {
+        }
+
+        public JobException(String s) : base(s)
+        {
+        }
+
+        public JobException(Node n, String s) : this(n.ToString() + s)
+        {
+        }
+    }
+}
