f114e2d754c49fdf38b8c8ef3305612bbbe11a2b
feupeu <nkmouritzsen@gmail.com>
2014-12-16 17:30:24 +0100

    Code cleanup in NavigateJob.cs

--- BTCom/BTCom/NavigateJob.cs
+++ BTCom/BTCom/NavigateJob.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using BTCom.Exceptions;
 
 namespace BTCom
@@ -11,11 +10,9 @@ namespace BTCom
     public class NavigateJob : Job
     {
         public Node Destination { get; set; }
-        public string Directions { get; set; }
 
         public NavigateJob()
         {
-            Directions = String.Empty;
             Destination = null;
         }
 
@@ -24,74 +21,13 @@ namespace BTCom
             Identifier = identifier;
         }
 
-        public NavigateJob(int identifier, String directions) : this(identifier)
-        {
-            string dir = directions.ToUpper();
-
-            Regex lrsRegex = new Regex("^[LRS]*$");
-
-            if (!lrsRegex.IsMatch(dir))
-            {
-                throw new Exception("Could not convert '" + Directions + "' to path");
-            }
-
-            Directions = dir;
-        }
-
         public NavigateJob(int identifier, Node destination) : this(identifier)
         {
             Destination = destination;
         }
 
-        private static readonly List<List<Char>> navigationMatrix = new List<List<char>>()
-        {
-            new List<Char>() { '#','L','S','R' }, // 0
-            new List<Char>() { 'R','#','L','S' }, // 1
-            new List<Char>() { 'S','R','#','L' }, // 2
-            new List<Char>() { 'L','S','R','#' }  // 3
-        };
-
         public override Path GetPath()
         {
-            if (Directions != String.Empty)
-            {
-                Forklift f = Database.Instance.Data.Forklifts.FirstOrDefault().Value;
-
-                Node frontNode = f.FrontNode;
-                Node rearNode = f.RearNode;
-
-                Path p = new Path();
-
-                foreach (char character in Directions.ToList())
-                {
-                    int index = frontNode.Neighbours.FindIndex(x => x.Key != null && x.Key.Equals(rearNode));
-
-                    int outIndex = navigationMatrix[index].FindIndex(x => x.Equals(character));
-
-                    Node newFrontNode = frontNode.Neighbours[outIndex].Key;
-                    Node newRearNode = frontNode;
-
-                    if (newFrontNode == null)
-                    {
-                        throw new Exception("Cannot create path with directions '" + Directions + "'. Cannot do '" + character + "' in node '" + frontNode.Name + "' (from '" + rearNode.Name + "')");
-                    }
-
-                    p.Nodes.Add(frontNode);
-
-                    frontNode = newFrontNode;
-                    rearNode = newRearNode;
-                }
-
-                p.Nodes.Add(frontNode);
-
-                if (p.Nodes.Last().Equals(f.FrontNode))
-                {
-                    throw new JobException("PALL-E already at destination '" + f.FrontNode.Name + "'");
-                }
-
-                return p;
-            }
-            
             if(Destination != null)
             {
                 Graph g = Database.Instance.Data.Graphs.FirstOrDefault().Value;
@@ -141,10 +77,9 @@ namespace BTCom
             // Compare properties of both objects
             var sameIdentifier = this.Identifier == other.Identifier;
             var sameDestination = this.Destination.Equals(other.Destination);
-            var sameDirectionns = this.Directions.Equals(other.Directions);
 
             // Check if all properties are the same
-            return sameIdentifier && sameDestination && sameDirectionns;
+            return sameIdentifier && sameDestination;
         }
 
         public override string ToString()
@@ -154,11 +89,6 @@ namespace BTCom
             if (Destination != null)
             {
                 return s + ". Destination: '" + Destination.Name + "'";
-            }
-            
-            if (Directions != String.Empty)
-            {
-                return s + ". Directions: '" + Directions.ToUpper() + "'";
             }
 
             return s;
