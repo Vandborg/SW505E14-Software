61d742f4902b3e95419bdd04f0d6918f008aa046
feupeu <nkmouritzsen@gmail.com>
2014-12-16 17:27:11 +0100

    Code cleanup in ForkliftPath.cs

--- BTCom/BTCom/ForkliftPath.cs
+++ BTCom/BTCom/ForkliftPath.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 using BTCom.Exceptions;
 
 namespace BTCom
@@ -11,7 +8,7 @@ namespace BTCom
     public class ForkliftPath
     {
         public static readonly char[,] navigationMatrix = new char[4, 4] { {'#','L','S','R'}, {'R','#','L','S'},
-                                                                          {'S','R','#','L'}, {'L','S','R','#'} };
+                                                                           {'S','R','#','L'}, {'L','S','R','#'} };
 
         private readonly List<Char> forkliftPath = new List<Char>();
 
@@ -46,10 +43,12 @@ namespace BTCom
             }
 
             // Check if the initial node is a neighbour of the first node in the path
-            if (path.Nodes.FirstOrDefault().Neighbours.FindAll(x => x.Key != null && x.Key.Equals(this.initialNode)).Count == 0
-                && path.Nodes.FirstOrDefault().BlockedNeighbours.FindAll(x => x.Key != null && x.Key.Equals(this.initialNode)).Count == 0)
+            Node node = path.Nodes.FirstOrDefault();
+            if (node != null 
+                && (node.Neighbours.FindAll(x => x.Key != null && x.Key.Equals(this.initialNode)).Count == 0
+                && node.BlockedNeighbours.FindAll(x => x.Key != null && x.Key.Equals(this.initialNode)).Count == 0))
             {
-                throw new NodeException("Could not find initial node '" + this.initialNode.Name + "' in the neighbours of node '" + path.Nodes.FirstOrDefault().Name + "'");
+                throw new NodeException("Could not find initial node '" + this.initialNode.Name + "' in the neighbours of node '" + node.Name + "'");
             }
 
             this.path = path;
